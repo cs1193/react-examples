9e45561fb2cacd9e0eb8cad0b18c9f0b419a968d
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2017-05-18 01:46:47 +0530

    React Webpack Setup

---
+++ example/example.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleApp from '../source/index';
+
+ReactDOM.render(<ExampleApp />, document.getElementById('example'));
---
+++ source/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class ExampleApp extends Component {
+  render () {
+    return (
+      <div>Example</div>
+    );
+  }
+}
